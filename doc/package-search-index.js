4910580df1f200ce85956bb4c0e88318307a4d72
standardProton <alexm024680@gmail.com>
2024-08-08 02:24:51 -0600

    Javadocs initial commit

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"me.c7dev.dexterity.api"},{"l":"me.c7dev.dexterity.api.events"},{"l":"me.c7dev.dexterity.displays"},{"l":"me.c7dev.dexterity.displays.animation"},{"l":"me.c7dev.dexterity.transaction"},{"l":"me.c7dev.dexterity.util"}];updateSearchResults();
